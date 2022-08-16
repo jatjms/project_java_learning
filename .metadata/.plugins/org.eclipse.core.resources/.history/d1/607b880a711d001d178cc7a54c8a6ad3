803f59e3b8b3ecd12933363341440ca9884c1ba5
jatjms <jhonatanat@gmail.com>
2022-08-16 14:51:16 -0300

    Aprendizado

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/d1/607b880a711d001d178cc7a54c8a6ad3
@@ -0,0 +1,14 @@
+package com.jatsys.repetitionStructure;
+
+import java.util.Scanner;
+//import java.lang.*;
+
+
+public class Exercise03 {
+	public static void main(String[] args) {
+		try(Scanner scan = new Scanner(System.in)){
+			
+		}
+	}
+
+}
--- jatsysti-app/src/main/java/com/jatsys/repetitionStructure/Exercise03.java
+++ jatsysti-app/src/main/java/com/jatsys/repetitionStructure/Exercise03.java
@@ -5,6 +5,14 @@ import java.util.Scanner;
 
 
 public class Exercise03 {
+	/*
+	 * Faça um programa que leia e valide as seguintes informações:
+	 * a. Nome: maior que 3 caracteres;
+	 * b. Idade: entre 0 e 150;
+	 * c. Salário: maior que zero;
+	 * d. Sexo: 'f' ou 'm';
+	 * e. Estado Civil: 's', 'c', 'v', 'd';
+	 */
 	public static void main(String[] args) {
 		try(Scanner scan = new Scanner(System.in)){
 			
