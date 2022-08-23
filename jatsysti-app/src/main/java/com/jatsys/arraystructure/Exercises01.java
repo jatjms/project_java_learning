a958ac530f7ca6901cfe079adfb6549fdd82c7a5
jatjms <jhonatanat@gmail.com>
2022-08-23 09:57:58 -0300

    Aprendizado

---
+++ jatsysti-app/src/main/java/com/jatsys/arraystructure/Exercises01.java
@@ -0,0 +1,9 @@
+package com.jatsys.arraystructure;
+
+public class Exercises01 {
+	public static void main(String[] args) {
+		System.out.println("Hello Word!!");
+		System.out.println("Olá Mundo!!");
+	}
+
+}
